ea9e6272e8097d6b0f7874de5cec63341c0c99ba
Michael Koontz <mikoontz@gmail.com>
2021-12-17 10:29:42 -0800

    drop .geo column

--- read-fire-severity-samples.R
+++ read-fire-severity-samples.R
@@ -2,4 +2,10 @@ library(dplyr)
 library(sf)
 library(data.table)
 
-samps <- sf::st_read("data/out/ee/firesamples_48days_Landsat4578.geojson")
+samps <- data.table::fread("data/out/ee/great-basin-fire-productivity-efficiency-sensitivity-severity-calculations.csv")
+samps[, .geo := NULL]
+
+samps
+summary(samps)
+
+data.table::fwrite(x = samps, file = "data/out/fire-severity-at-sample-points_48-days.csv")
